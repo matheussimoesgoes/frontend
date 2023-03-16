ce165a9efc236547ed5ca7c14c9ed1da2aed2387
matheussimoesgoes <matheussimoesgoes@gmail.com>
2023-03-16 19:42:38 -0300

    atividade pronta

--- 2-frontend/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/src/main.js
+++ 2-frontend/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/src/main.js
@@ -1,1 +1,20 @@
 import validator from 'validator';
+import './style.css';
+
+const textoEscrito = document.getElementById('texto');
+const botao = document.getElementById('validar');
+const seletor = document.getElementById('dados');
+const saida = document.getElementById('saida');
+
+botao.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const resultado = {
+    cpf: validator.isTaxID(textoEscrito.value, 'pt-BR'),
+    email: validator.isEmail(textoEscrito.value),
+    telefone: validator.isNumeric(textoEscrito.value),
+    nome: validator.isAlpha(textoEscrito.value, 'pt-BR'),
+    rg: validator.isLength(textoEscrito.value, { min: 11, max: 11 }),
+  };
+  saida.innerHTML = `A validação retornou ${resultado[seletor.value]}`;
+});
